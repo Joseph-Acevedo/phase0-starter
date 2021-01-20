c78f794f16e91c407b04bbea4b15420c8aa31486
Joseph Acevedo <joseph.acevedo@outlook.com>
2021-01-20 16:43:12 -0700

    Added all necessary code for phase0

--- phase0.c
+++ phase0.c
@@ -1,9 +1,44 @@
+/*
+ * Name: Joseph Acevedo
+ * NetID: josephacevedo
+ * Phase0
+ */
+
 #include <stdio.h>
 #include "usloss.h"
 
+#define SIZE (USLOSS_MIN_STACK)
+#define MAX_ITER 10
 
-void
-startup(int argc, char **argv)
+USLOSS_Context helloCtxt;
+USLOSS_Context worldCtxt;
+
+char stack0[SIZE];
+char stack1[SIZE];
+
+
+void hello(void) {
+     int i = 0;
+     while (++i <= MAX_ITER) {
+	USLOSS_Console("%d Hello ", i);
+	USLOSS_ContextSwitch(&helloCtxt, &worldCtxt);
+     }
+     USLOSS_Halt(0);
+}
+
+void world(void) {
+     int i = 0;
+     while (++i <= MAX_ITER) {
+	USLOSS_Console("World");
+	int j = i;
+	while (j-- > 0)
+            USLOSS_Console("!");
+	USLOSS_Console("\n");
+	USLOSS_ContextSwitch(&worldCtxt, &helloCtxt);
+     }
+}
+
+void startup(int argc, char **argv)
 {
     /*
      * Your code here. If you compile and run this as-is you
@@ -11,24 +46,24 @@ startup(int argc, char **argv)
      * to verify that you get a core file and you can use it
      * with gdb.
      */
+     USLOSS_ContextInit(&helloCtxt, stack0, sizeof(stack0), NULL, hello);
+     USLOSS_ContextInit(&worldCtxt, stack1, sizeof(stack1), NULL, world);
+     USLOSS_ContextSwitch(NULL, &helloCtxt);
 }
 
 // Do not modify anything below this line.
 
-void
-finish(int argc, char **argv)
+void finish(int argc, char **argv)
 {
     USLOSS_Console("Goodbye.\n");
 }
 
-void
-test_setup(int argc, char **argv)
+void test_setup(int argc, char **argv)
 {
     // Do nothing.
 }
 
-void
-test_cleanup(int argc, char **argv)
+void test_cleanup(int argc, char **argv)
 {
     // Do nothing.
 }
